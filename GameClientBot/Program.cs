0bb11aaf81475ded1ca098a0e7a4b4d7b376ef05
Sohaib Cheroud <cheroud.sohaib@gmail.com>
2025-11-15 18:06:53 +0100

    Create Bot project

---
+++ GameClientBot/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+record Player(Guid Id, string Username, string ColorHex);
+record JoinRequest(string Username, string ColorHex);
+record JoinResponse(Guid PlayerId);
+record ActionRequest(Guid PlayerId, string Command);
